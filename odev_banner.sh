6b2a83051748498cd37d29294cc2288e6f2c1073
Javier Moya <javier.moya@icloud.com>
2026-09-22 21:18:49 +0200

    Add banner

---
+++ odev_banner.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# format
+bold=$(tput bold)
+italic=$(tput sitm 2>/dev/null || true)
+normal=$(tput sgr0)
+
+echo ""
+echo "+---------------------------------------------------+"
+echo "|                                                   |"
+echo "|                                                   |"
+echo "|                       OREOL                       |"
+echo "|                                                   |"
+echo "|              Heterogenius computing               |"
+echo "|                                                   |"
+echo "|                                                   |"
+echo "|   Copyright (C) OREOL KLG. All rights reserved.   |"
+echo "|                                                   |"
+echo "+---------------------------------------------------+"
