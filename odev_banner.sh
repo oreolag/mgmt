b9cc6e4d5c00bbe001c32732f997a42840c0d569
Javier Moya <javier.moya@icloud.com>
2026-09-23 07:20:23 +0200

    Adding center()

--- odev_banner.sh
+++ odev_banner.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# format
 bold=$(tput bold)
 italic=$(tput sitm 2>/dev/null || true)
 normal=$(tput sgr0)
 
+year=$(date +%Y)
+
+center() {
+    local text=$1
+    local formatted=${2:-$1}
+    local left=$(( (51 - ${#text}) / 2 ))
+    local right=$(( 51 - ${#text} - left ))
+    printf '|%*s%s%*s|\n' "$left" '' "$formatted" "$right" ''
+}
+
 echo ""
 echo "+---------------------------------------------------+"
-echo "|                                                   |"
-echo "|                                                   |"
-echo "|                       OREOL                       |"
-echo "|                                                   |"
-echo "|              Heterogenius computing               |"
-echo "|                                                   |"
-echo "|                                                   |"
-echo "|   Copyright (C) OREOL KLG. All rights reserved.   |"
-echo "|                                                   |"
+center ""
+center ""
+center "OREOL Heterogenius computing" "${bold}OREOL${normal} Hetero${italic}genius${normal} computing"
+center ""
+center "To start using your cluster," "${italic}To start using your cluster,${normal}"
+center "type odev to explore the available commands." "${italic}type odev to explore the available commands.${normal}"
+center ""
+center "(C) $year OREOL KLG. All rights reserved."
+center ""
+center ""
 echo "+---------------------------------------------------+"
+#echo ""
